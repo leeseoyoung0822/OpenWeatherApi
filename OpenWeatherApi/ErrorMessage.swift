3c5edf7122a77abf75a569a2eca982c3c78edb9e
leeseoyoung0822 <822angel@naver.com>
2023-06-08 15:01:18 +0900

    Initial Commit

---
+++ OpenWeatherApi/ErrorMessage.swift
@@ -0,0 +1,14 @@
+//
+//  ErrorMessage.swift
+//  OpenWeatherApi
+//
+//  Created by 이서영 on 2023/06/07.
+//
+
+import Foundation
+import SwiftUI
+
+struct ErrorMessage: Codable {
+    let message: String
+}
+
